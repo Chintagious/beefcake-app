71553b2a075cbf501b7a411b869bc4a85891f564
Chintan Shah <1408437+Chintagious@users.noreply.github.com>
2017-10-29 11:25:50 -0400

    Initial vanilla react native commit

--- App.js
+++ App.js
@@ -1,36 +1,38 @@
-import React from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  View } from 'react-native';
-import { Container, Header, Content, Card, CardItem, Body, Button, Text, Right } from 'native-base';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
 
-export default class App extends React.Component {
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component<{}> {
   render() {
     return (
-      <Container>
-        <Header />
-        <Content>
-          <Card>
-            <CardItem>
-              <Text>
-                WHAT UP DAWGS?{"\n"}
-                Open up App.js to start working on your app!{"\n"}
-                Changes you make will automatically reload.{"\n"}
-                Shake your phone to open the developer menu.
-              </Text>
-            </CardItem>
-            <CardItem>
-              <Body />
-              <Right>
-                <Button>
-                  <Text>A button, yo</Text>
-                </Button>
-              </Right>
-            </CardItem>
-          </Card>
-        </Content>
-      </Container>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          What up, dawgs? Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+      </View>
     );
   }
 }
@@ -38,10 +40,18 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
   },
 });
-
-AppRegistry.registerComponent('App', () => App);
